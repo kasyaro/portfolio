1c5ebc18310d364ba07f6b84051503890b4a3fe9
Alexandra Romanova <alexandraromanova@Alexandras-MacBook-Air.local>
2019-06-06 16:45:43 -0400

    set up header bootstrup

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 //console.log( "hello world")
 
+ 
+//     const $cardback = $('.card-back')
+//     $('.card-back').append('<p> About</p>')
+
+
+// $(() => {
+// const $card = $('.card')
+//  $card.on('click', ()=>{
+//     $('.card').toggleClass('card-back')
+// })
+// })
+// $('.card').toggleClass('card-back') === $card.toggleClass('card-back')
+
+
+// const Display = {
+// 	onClickAddParagraph: () => {
+// 		$('section').append('<p>About</p>');
+// 	}
+// }
